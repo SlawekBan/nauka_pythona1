8b0c8c59c1f03dce9f38a5c21a63516ffe533d1c
SlawekBan <SlawekBan@users.noreply.github.com>
2018-12-09 11:02:31 +0100

    trzeci program

---
+++ trzeci_program.py
@@ -0,0 +1,12 @@
+asortyment = 'Telewizor'
+opakowanie = 'Karton '
+cale = 42
+karton1 = 'szary'
+karton2 = 'bialy'
+
+print("Telewizor potrzebuje " + opakowanie + str(cale))
+
+if asortyment > 42:
+    print(karton1)
+else:
+    print(karton2)
