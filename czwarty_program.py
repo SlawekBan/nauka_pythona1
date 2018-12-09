0975cec01bbe7a59ba9a6d114fb1f9c6189a1e24
SlawekBan <SlawekBan@users.noreply.github.com>
2018-12-09 11:20:59 +0100

    zmiana w czwartym programie

--- czwarty_program.py
+++ czwarty_program.py
@@ -1,5 +1,5 @@
 produkt = 'mleko'
-suma = 150
+suma = 301
 
 if 100 < suma and suma < 200:
     print('Promocja 30%!')
@@ -7,6 +7,9 @@ if 100 < suma and suma < 200:
 elif suma >= 200:
     print('Promocja 35%')
     suma = suma - (suma * 35) / 100
+elif suma >= 300:
+    print('Pomocja 50%')
+    suma = suma - (suma * 50) / 100
 else:
     print('bez promocji!')
 
