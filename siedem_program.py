2c3be9e3c34d1fa3e64dfb290633d585a23e5924
SlawekBan <SlawekBan@users.noreply.github.com>
2018-12-09 13:29:06 +0100

    zmiana w siedem

--- siedem_program.py
+++ siedem_program.py
@@ -1,6 +1,13 @@
 samolot = {'name': 'boeing',
             'przebieg': 10000,
             'type': 'pasazerski'}
+
+#print(samolot['name'])
+#print(samolot['type'])
+
+# co wypiszemy na ekran
+#print(samolot['nieznany_klucz'])
+
 # in python 3 samolot.item()
 for key, value in samolot.iteritems():
     print("{0}:{1}".format(key, value))
